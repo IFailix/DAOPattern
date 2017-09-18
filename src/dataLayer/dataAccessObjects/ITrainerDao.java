fb9404948b67de5213fa7499d6942c8deff36615
Failix <321Faily@gmail.com>
2017-09-18 10:53:25 +0200

    (018588)added ITrainerDao interface

---
+++ src/dataLayer/dataAccessObjects/ITrainerDao.java
@@ -0,0 +1,25 @@
+package dataLayer.dataAccessObjects;
+
+import java.util.ArrayList;
+
+public interface ITrainerDao {
+
+    public ITrainer create();
+
+    public void delete(Trainer ITrainer);
+
+    public ITrainer first();
+
+    public ITrainer last();
+
+    public ITrainer next(Trainer ITrainer);
+
+    public ITrainer previous(Trainer ITrainer);
+
+    public void save(Trainer ITrainer);
+
+    public ArrayList select();
+
+    public ITrainer select(int id);
+
+}
